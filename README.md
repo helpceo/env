b58aaa42b2b62316119bac7f08da5853ec445f8f
helpceo <helpceo@126.com>
2018-08-01 16:37:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## javaScript环境变量
+
+### 使用方法
+```
+import 'env'
+env('name')
+```
---
+++ env.js
@@ -0,0 +1,7 @@
+import fs from "fs";
+global.env = (item)=>{
+    let env = JSON.parse(fs.readFileSync(__dirname+'/.env', 'utf8', function(err, data){
+        console.log(err)
+    }));
+    return env[item];
+}
